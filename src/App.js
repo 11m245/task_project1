e8e15ef991600e951c153fb95e8ccfd4e30cee7b
11m245 <sivaraj2siva@gmail.com>
2022-11-18 14:33:09 +0530

    now2

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function MovieComponent({ image_url: image_u, title, rating, summary }) {
       <img src={image_u} alt="movie" />
       <div className='content-wrapper'>
         <div className="line-w">
-          <h3 className='title'>{title}</h3>
+          <h3 className='title'>{title}.</h3>
           <div className="rating-w">
             <img className='star' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Star%2A.svg/2234px-Star%2A.svg.png" alt="star" />
             <h3 className='rating'>{rating}</h3>
